36f62160a2b4f4685d9d1d317d8d898d39282016
WaerWaters <daniel.johnsen1@hotmail.com>
2022-04-22 10:02:36 +0200

    object

--- coding/js/default.js
+++ coding/js/default.js
@@ -1,8 +1,8 @@
 let editor;
 
 $(document).ready(function() {
-    let code = $(".editor")[0];
-    editor = CodeMirror.fromTextArea(code, {
+    let code01 = $(".editor")[0];
+    editor = CodeMirror.fromTextArea(code01, {
         mode: "xml",
         theme: "dracula",
         lineNumbers: true,
@@ -10,6 +10,13 @@ $(document).ready(function() {
         autoCloseTags: true,
         autoCloseBrackets: true,
         extraKeys: {"Ctrl": "autocomplete"},
+    });
+    let example = $(".example")[0];
+    help = CodeMirror.fromTextArea(example, {
+        mode: "xml",
+        theme: "dracula",
+        lineNumbers: true,
+        tabSize: 3,
     });
 });
 
@@ -22,7 +29,8 @@ function check() {
 
 function clear(clearCondition) {
     if (clearCondition == "<h1>Hello World!</h1>") {
-        document.getElementById("check-box").hidden = false
         document.getElementById("first-condition").style = "background-color: green; border-bottom: solid; border-top: solid;"
+    } else {
+        document.getElementById("first-condition").style = "background-color: red; border-bottom: solid; border-top: solid;"
     }
 }
